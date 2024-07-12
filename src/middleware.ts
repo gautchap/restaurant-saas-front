7fd54efff7adb60c745c9f6992c94e60bc12c581
gautchap <gautchap+github@outlook.com>
2024-07-12 13:05:37 +0200

    feat: create middleware to handle authentication

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export const middleware = auth(async (request) => {
+    if (!request.auth && request.nextUrl.pathname !== "/login") {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    if (request.auth && request.nextUrl.pathname === "/login") {
+        return NextResponse.redirect(new URL("/account", request.url));
+    }
+
+    if (!request.auth && request.nextUrl.pathname === "/login") {
+        return NextResponse.next();
+    }
+
+    const res = await fetch(`${process.env.BACKEND_URL}/auth/me`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${request.auth?.accessToken}`,
+        },
+        cache: "no-store",
+    });
+
+    if (res.status === 401) {
+        return NextResponse.redirect(new URL("/signout", request.url));
+    }
+
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: ["/login", "/account/:path*"],
+};

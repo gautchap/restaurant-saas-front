8b904530980fe4a3d4c2bbcdde22bf3755806038
gautchap <gautchap+github@outlook.com>
2024-07-13 00:34:09 +0200

    refactor: change items types and list items

--- src/types/itemSchema.ts
+++ src/types/itemSchema.ts
@@ -2,10 +2,12 @@ import { z } from "zod";
 
 export const itemSchema = z.object({
     id: z.string().uuid(),
-    type: z.string(),
+    type: z.enum(["table", "chair"]),
+    name: z.string(),
     userId: z.number(),
     x: z.number(),
     y: z.number(),
+    shape: z.enum(["rectangle", "circle"]),
     new: z.boolean().optional(),
 });
 
--- src/utils/list-items.ts
+++ src/utils/list-items.ts
@@ -1,7 +1,10 @@
+import type { Item } from "@/types/itemSchema";
 import { nanoid } from "nanoid";
 
+type ListItemsType = Omit<Item, "userId" | "x" | "y">;
+
 export const ListItems = [
-    { id: nanoid(3), type: "Table" },
-    { id: nanoid(3), type: "Table 2" },
-    { id: nanoid(3), type: "Chaise" },
-];
+    { id: nanoid(3), type: "table", name: "Table 1", shape: "rectangle" },
+    { id: nanoid(3), type: "table", name: "Table 2", shape: "circle" },
+    { id: nanoid(3), type: "chair", name: "Chaise", shape: "circle" },
+] satisfies ListItemsType[];

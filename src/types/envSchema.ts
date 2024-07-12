f61bc0f335dbed3da24986baf0905e2ed912f2a2
gautchap <gautchap+github@outlook.com>
2024-07-12 12:37:45 +0200

    feat : add env validation

---
+++ src/types/envSchema.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+
+const envSchema = z.object({
+    AUTH_GITHUB_ID: z.string().trim().min(1),
+    AUTH_GITHUB_SECRET: z.string().trim().min(1),
+    AUTH_URL: z.string().trim().min(1).url(),
+    AUTH_SECRET: z.string().trim().min(1),
+    PORT: z.number().default(3000),
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+});
+
+const envServer = envSchema.safeParse({
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+    AUTH_URL: process.env.AUTH_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+});
+
+if (!envServer.success) {
+    console.error(envServer.error.issues);
+    throw new Error("There is an error with the server environment variables");
+    process.exit(1);
+}
+
+export const envServerSchema = envServer.data;
+export type EnvSchemaType = z.infer<typeof envSchema>;

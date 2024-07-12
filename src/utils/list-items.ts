ea1fec3acc4765fa397aa7f0eed6201dfd43ceca
gautchap <gautchap+github@outlook.com>
2024-07-12 15:35:26 +0200

    feat : add nanoid and create list of default items

---
+++ src/utils/list-items.ts
@@ -0,0 +1,7 @@
+import { nanoid } from "nanoid";
+
+export const ListItems = [
+    { id: nanoid(3), type: "Table" },
+    { id: nanoid(3), type: "Table 2" },
+    { id: nanoid(3), type: "Chaise" },
+];

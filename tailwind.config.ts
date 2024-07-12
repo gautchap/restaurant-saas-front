534371250106763123099da85891df490650d86b
gautchap <gautchap+github@outlook.com>
2024-07-12 13:43:31 +0200

    chore : add tailwind animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            keyframes: {
+                pop: {
+                    from: { transform: "translate3d(var(--translate-x, 0), var(--translate-y, 0), 0) scale(1)" },
+                    to: {
+                        transform: "translate3d(var(--translate-x, 0), var(--translate-y, 0), 0) scale(1.06)",
+                    },
+                },
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                "dots-craft": "radial-gradient(circle, grey 1px, rgba(0, 0, 0, 0) 1px)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [require("tailwindcss-animate")],
 };
 export default config;

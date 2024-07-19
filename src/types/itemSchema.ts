13686e9bbb0d3e22a950952f0778a98d5fdf5334
gautchap <gautchap+github@outlook.com>
2024-07-19 16:30:09 +0200

    refactor : update itemSchema with tableNumber and chairAmount

--- src/types/itemSchema.ts
+++ src/types/itemSchema.ts
@@ -4,11 +4,13 @@ export const itemSchema = z.object({
     id: z.string().uuid(),
     type: z.enum(["table", "chair"]),
     name: z.string(),
-    userId: z.number(),
+    userId: z.string().uuid(),
     x: z.number(),
     y: z.number(),
-    shape: z.enum(["rectangle", "circle"]),
+    shape: z.enum(["rectangle", "circle", "multi"]),
     new: z.boolean().optional(),
+    tableNumber: z.number().optional(),
+    chairAmount: z.number().optional(),
 });
 
 export type Item = z.infer<typeof itemSchema>;

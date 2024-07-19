21cd524bc33f908fe041408bae861fb5b396e1f4
gautchap <gautchap+github@outlook.com>
2024-07-19 16:22:23 +0200

    refactor : change style

--- src/components/lock-toggle.tsx
+++ src/components/lock-toggle.tsx
@@ -9,9 +9,11 @@ type LockToggleProps = {
 export function LockToggle({ isDisabled, ...props }: LockToggleProps) {
     return (
         <Button {...props} title="switch floor lock" variant="ghost" size="icon">
-            <Lock className={`size-[1.2rem] transform-gpu transition-all ${isDisabled ? "scale-100" : "scale-0"}`} />
+            <Lock
+                className={`size-[1.2rem] transform-gpu transition-transform ${isDisabled ? "scale-100" : "scale-0"}`}
+            />
             <LockOpen
-                className={`absolute size-[1.2rem] transform-gpu transition-all ${isDisabled ? "scale-0" : "scale-100"}`}
+                className={`absolute size-[1.2rem] transform-gpu transition-transform ${isDisabled ? "scale-0" : "scale-100"}`}
             />
         </Button>
     );

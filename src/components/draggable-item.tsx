ba43dc7ffd13e4f9a82156afdf286c515f044937
gautchap <gautchap+github@outlook.com>
2024-07-19 16:24:14 +0200

    refactor: add tableNumber and chairAmount props

--- src/components/draggable-item.tsx
+++ src/components/draggable-item.tsx
@@ -4,6 +4,7 @@ import type { UniqueIdentifier } from "@dnd-kit/core";
 import type { Item } from "@/types/itemSchema";
 import { Draggable } from "@/components/draggable";
 import { useDraggable } from "@dnd-kit/core";
+import { memo } from "react";
 
 interface DraggableItemProps {
     label: string;
@@ -13,27 +14,33 @@ interface DraggableItemProps {
     id: UniqueIdentifier;
     isList?: boolean;
     shape: Item["shape"];
+    tableNumber?: number;
+    chairAmount?: number;
 }
 
-export function DraggableItem({ label, top, left, disabled, id, isList, shape }: DraggableItemProps) {
-    const { attributes, isDragging, listeners, setNodeRef, transform } = useDraggable({
-        id,
-        disabled,
-    });
+export const DraggableItem = memo(
+    ({ label, top, left, disabled, id, isList, shape, tableNumber, chairAmount }: DraggableItemProps) => {
+        const { attributes, isDragging, listeners, setNodeRef, transform } = useDraggable({
+            id,
+            disabled,
+        });
 
-    return (
-        <Draggable
-            id={id}
-            isList={isList}
-            disabled={disabled}
-            ref={setNodeRef}
-            dragging={isDragging}
-            label={label}
-            listeners={listeners}
-            shape={shape}
-            style={{ top, left }}
-            transform={transform}
-            {...attributes}
-        />
-    );
-}
+        return (
+            <Draggable
+                tableNumber={tableNumber}
+                chairAmount={chairAmount}
+                id={id}
+                isList={isList}
+                disabled={disabled}
+                ref={setNodeRef}
+                dragging={isDragging}
+                label={label}
+                listeners={listeners}
+                shape={shape}
+                style={{ top, left }}
+                transform={transform}
+                {...attributes}
+            />
+        );
+    }
+);

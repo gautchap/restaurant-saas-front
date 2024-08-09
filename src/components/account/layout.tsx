d7d2e9c4e3dd0897fc11dd5a903bbe5c41ae5519
gautchap <gautchap+github@outlook.com>
2024-08-09 23:58:47 +0200

    refactor: change style

--- src/components/account/layout.tsx
+++ src/components/account/layout.tsx
@@ -8,6 +8,13 @@ export default function Layout({ children }: { children: ReactNode }) {
     const pathName = usePathname();
 
     return (
-        <div className={cn("flex-1", pathName.includes("floor") ? "pl-5" : "overflow-auto container")}>{children}</div>
+        <div
+            className={cn(
+                "flex-1",
+                pathName.includes("floor") ? "pl-5" : "overflow-auto container px-4 md:px-8 pt-10 md:pt-7"
+            )}
+        >
+            {children}
+        </div>
     );
 }

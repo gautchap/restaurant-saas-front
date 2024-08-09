c7579f8f8c9d292384e599ef7abd5d607749d9ad
gautchap <gautchap+github@outlook.com>
2024-08-09 16:16:45 +0200

    feat : add new animation properties to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,6 +78,9 @@ const config = {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
+            transitionDuration: {
+                "2500": "2500ms",
+            },
         },
     },
     plugins: [require("tailwindcss-animate")],

b6d65d8de1162a2abd316e4c0260a5ff25c22ab8
gautchap <gautchap+github@outlook.com>
2024-08-09 16:14:24 +0200

    refactor: add action for user update

--- src/actions/booking.ts
+++ src/actions/booking.ts
@@ -5,16 +5,6 @@ import { actionClient } from "@/lib/safe-action";
 import { bookingSchema } from "@/types/bookingSchema";
 import { fetcher } from "@/utils/fetcher";
 
-export const postBooking = actionClient.schema(bookingSchema).action(async ({ parsedInput }) => {
-    const res = await fetcher(`${process.env.BACKEND_URL}/bookings/create`, {
-        method: "POST",
-        data: { booking: parsedInput },
-        zodSchema: bookingSchema,
-    });
-
-    return res;
-});
-
 export async function getBookings() {
     const session = await auth();
     if (!session) return;
@@ -30,3 +20,29 @@ export async function getBookings() {
 
     return res;
 }
+
+export const postBookingPublic = actionClient.schema(bookingSchema).action(async ({ parsedInput }) => {
+    const res = await fetcher(`${process.env.BACKEND_URL}/bookings/create/public`, {
+        method: "POST",
+        data: { booking: parsedInput },
+        zodSchema: bookingSchema,
+        revalidate: "bookings",
+    });
+
+    return res;
+});
+
+export const postBookingPrivate = actionClient.schema(bookingSchema).action(async ({ parsedInput }) => {
+    const session = await auth();
+    if (!session) return;
+
+    const res = await fetcher(`${process.env.BACKEND_URL}/bookings/create/private`, {
+        method: "POST",
+        headers: { Authorization: `Bearer ${session?.accessToken}` },
+        data: { booking: parsedInput },
+        zodSchema: bookingSchema,
+        revalidate: "bookings",
+    });
+
+    return res;
+});
--- src/types/bookingSchema.ts
+++ src/types/bookingSchema.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const bookingSchema = z.object({
     userId: z.string().uuid(),
-    id: z.string().uuid().optional(),
+    id: z.string().uuid().default("3e7b25d1-fb5e-4612-b604-ad101f39a5b9"),
+    assigned: z.string().uuid().optional().nullable(),
     date: z.coerce.string().datetime({ offset: true }),
     tel: z.string().optional(),
     persons: z.number(),
@@ -10,6 +11,9 @@ export const bookingSchema = z.object({
     lastName: z.string(),
     email: z.string().email(),
     message: z.string().optional().nullable(),
+    status: z.enum(["confirmed", "cancelled", "completed"]).default("confirmed"),
 });
 
-export type Boonking = z.infer<typeof bookingSchema>;
+export const bookingSchemaEdit = bookingSchema.omit({ userId: true });
+
+export type Booking = z.infer<typeof bookingSchema>;

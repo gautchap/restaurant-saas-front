6682085b292d367eb9fd3bb628b9655a783f2d12
gautchap <gautchap+github@outlook.com>
2024-09-24 13:19:57 +0200

    refactor: disable async import

--- src/app/account/floor/page.tsx
+++ src/app/account/floor/page.tsx
@@ -1,23 +1,9 @@
-import dynamic from "next/dynamic";
 import { getItems } from "@/actions/items";
 import { Droppable } from "@/components/dnd-floor/droppable";
 import { DragNavbar } from "@/components/dnd-floor/drag-navbar";
 import { TrashDroppable } from "@/components/dnd-floor/trash-droppable";
 import { auth } from "@/lib/auth";
-
-const DraggableProvider = dynamic(
-    // eslint-disable-next-line github/no-then
-    () => import("@/context/draggable-provider").then((module_) => module_.DraggableProvider),
-    {
-        ssr: false,
-        loading: () => (
-            <div className="flex">
-                <div className="relative h-[calc(950px*0.8)] w-[calc(1920px*0.8)] border border-primary md:mx-auto" />
-                <div className="flex h-screen w-32 flex-col border-l border-l-primary/10 bg-secondary py-2 shadow-lg" />
-            </div>
-        ),
-    }
-);
+import { DraggableProvider } from "@/context/draggable-provider";
 
 export default async function Page() {
     const items = await getItems();

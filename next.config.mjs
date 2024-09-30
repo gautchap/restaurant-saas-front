b60d19e46c6de3b7a0798371ba812b6a156970c3
gautchap <gautchap+github@outlook.com>
2024-09-30 16:27:38 +0200

    ops: add redis to share cache across multiple instances of the app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    cacheHandler: process.env.NODE_ENV === "production" ? "./cache-handler.mjs" : undefined,
+    cacheMaxMemorySize: 0,
 };
 
 export default nextConfig;

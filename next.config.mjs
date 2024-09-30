ad9b2e99a1c5e05977ee7ff0d17a157c200fb66f
gautchap <gautchap+github@outlook.com>
2024-09-30 20:18:54 +0200

    refactor: allow cache for dev mode

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: "standalone",
     cacheHandler: process.env.NODE_ENV === "production" ? "./cache-handler.mjs" : undefined,
-    cacheMaxMemorySize: 0,
+    cacheMaxMemorySize: process.env.NODE_ENV === "production" ? 0 : 50_000_000,
 };
 
 export default nextConfig;
